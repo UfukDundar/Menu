f9c1dea8d3a5448636101b7a2940a12f9f1ed2ca
ufuk <ufuk-dundar@outlook.com>
2016-12-29 09:21:02 +0300

    Updated.

--- Menu/Form1.Designer.cs
+++ Menu/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbMenu = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstYemekler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmbMenu
+            // 
+            this.cmbMenu.FormattingEnabled = true;
+            this.cmbMenu.Location = new System.Drawing.Point(92, 143);
+            this.cmbMenu.Name = "cmbMenu";
+            this.cmbMenu.Size = new System.Drawing.Size(121, 24);
+            this.cmbMenu.TabIndex = 0;
+            this.cmbMenu.SelectedIndexChanged += new System.EventHandler(this.cmbMenu_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "C# Cafe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Menu:";
+            // 
+            // lstYemekler
+            // 
+            this.lstYemekler.FormattingEnabled = true;
+            this.lstYemekler.ItemHeight = 16;
+            this.lstYemekler.Location = new System.Drawing.Point(18, 196);
+            this.lstYemekler.Name = "lstYemekler";
+            this.lstYemekler.Size = new System.Drawing.Size(281, 324);
+            this.lstYemekler.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 556);
+            this.Controls.Add(this.lstYemekler);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbMenu);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstYemekler;
     }
 }
 
--- Menu/Form1.cs
+++ Menu/Form1.cs
@@ -16,5 +16,60 @@ namespace Menu
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbMenu.Items.Add("Başlangıç");
+            cmbMenu.Items.Add("Ara Sıcaklar");
+            cmbMenu.Items.Add("Ana Yemekler");
+            cmbMenu.Items.Add("Tatlılar");
+            cmbMenu.Items.Add("İçecekler");
+        }
+
+        private void cmbMenu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstYemekler.Items.Clear();
+            switch(cmbMenu.Text)
+            {
+                case "Başlancıç":
+                    {
+                        lstYemekler.Items.Add("Çorba");
+                        lstYemekler.Items.Add("Patates Kızartması");
+                        lstYemekler.Items.Add("Salata");
+                        lstYemekler.Items.Add("Çiğ Köfte");
+                        break;
+                    }
+                case "Ara Sıcaklar":
+                    {
+                        lstYemekler.Items.Add("Fındık Lahmacun");
+                        lstYemekler.Items.Add("İçli Köfte");
+                        break;
+                    }
+                case "Ana Yemekler":
+                    {
+                        lstYemekler.Items.Add("İskender");
+                        lstYemekler.Items.Add("Lahmacun");
+                        lstYemekler.Items.Add("Döner");
+                        break;
+                    }
+                case "Tatlılar":
+                    {
+                        lstYemekler.Items.Add("Tavuk Göğsü");
+                        lstYemekler.Items.Add("Baklava");
+                        lstYemekler.Items.Add("Kazandibi");
+                        lstYemekler.Items.Add("Tulumba");
+                        lstYemekler.Items.Add("Ekler");
+                        lstYemekler.Items.Add("Pasta");
+                        break;
+                    }
+                case "İçecekler":
+                    {
+                        lstYemekler.Items.Add("Ayran");
+                        lstYemekler.Items.Add("Kola");
+                        lstYemekler.Items.Add("Su");
+                        break;
+                    }
+            }
+        }
     }
 }
